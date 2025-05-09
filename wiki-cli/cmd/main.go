bea0534986a08835f372f1e6f44632274b4fc315
soyart <artdrawin@gmail.com>
2025-05-10 00:34:48 +0700

    init wiki-cli

---
+++ wiki-cli/cmd/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/alexflint/go-arg"
+	"github.com/soyart/far"
+)
+
+type cli struct {
+	RenameAsset *cmdRenameAsset `arg:"subcommand:rename-asset" help:"Rename 1 asset file and replace all references to the asset with the new filename"`
+}
+
+type cmdRenameAsset struct {
+	Old string `arg:"required,positional" help:"Old asset filename"`
+	New string `arg:"required,positional" help:"New asset filename"`
+}
+
+func main() {
+	c := cli{}
+	arg.MustParse(&c)
+
+	switch {
+	case c.RenameAsset != nil:
+		err := c.RenameAsset.run()
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func (c *cmdRenameAsset) run() error {
+	const basePath = "./assets"
+	pathOld := filepath.Join(basePath, c.Old)
+	pathNew := filepath.Join(basePath, c.New)
+
+	statOld, err := os.Stat(pathOld)
+	if err != nil {
+		return fmt.Errorf("failed to stat old path '%s': %w", pathOld, err)
+	}
+	if statOld.IsDir() {
+		return fmt.Errorf("path '%s' is a directory", pathOld)
+	}
+	_, err = os.Stat(pathNew)
+	if !os.IsNotExist(err) {
+		return fmt.Errorf("path '%s' already exists", pathNew)
+	}
+	data, err := os.ReadFile(pathOld)
+	if err != nil {
+		return fmt.Errorf("failed to read data for copying '%s': %w", pathOld, err)
+	}
+	err = os.WriteFile(pathNew, data, statOld.Mode().Perm())
+	if err != nil {
+		return fmt.Errorf("failed to copy data to '%s': %w", pathNew, err)
+	}
+	err = far.FindAndReplace("pages", c.Old, c.New)
+	if err != nil {
+		return fmt.Errorf("failed to find and replace in pages: %w", err)
+	}
+	err = far.FindAndReplace("journals", c.Old, c.New)
+	if err != nil {
+		return fmt.Errorf("failed to find and replace in journals: %w", err)
+	}
+	err = os.Remove(pathOld)
+	if err != nil {
+		return fmt.Errorf("failed to remove old file '%s': %w", pathOld, err)
+	}
+	return nil
+}
---
+++ wiki-cli/go.mod
@@ -0,0 +1,9 @@
+module github.com/framedb/wiki/wiki-cli
+
+go 1.24.2
+
+require (
+	github.com/alexflint/go-arg v1.5.1 // indirect
+	github.com/alexflint/go-scalar v1.2.0 // indirect
+	github.com/soyart/far v0.0.0-20250509170102-048390f6cf04 // indirect
+)
