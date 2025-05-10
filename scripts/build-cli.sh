71fa60b22fcb2fe2cb71fcd53dab8bbf559a601f
soyart <artdrawin@gmail.com>
2025-05-11 01:39:25 +0700

    flatten wiki-cli

--- scripts/build-cli.sh
+++ scripts/build-cli.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+echo "build-cli.sh: going into wiki-cli"
 cd wiki-cli
-go build ./cmd/main.go
+echo "build-cli.sh: building ./wiki-cli/cli to ./cli"
+go build ./main.go
 cp main ../cli
 rm main
 cd ..
+echo "build-cli.sh: done"
