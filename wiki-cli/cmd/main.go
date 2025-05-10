ff62cb59c58775f95154db1425ce1ac585ed516e
soyart <artdrawin@gmail.com>
2025-05-10 23:57:47 +0700

    wiki-cli: add new subcommands

--- wiki-cli/cmd/main.go
+++ wiki-cli/cmd/main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
+	"sort"
 
 	"github.com/alexflint/go-arg"
 	"github.com/soyart/far"
 )
 
 type cli struct {
-	RenameAsset *cmdRenameAsset `arg:"subcommand:rename-asset" help:"Rename 1 asset file and replace all references to the asset with the new filename"`
+	RenameAsset  *cmdRenameAsset  `arg:"subcommand:rename" help:"Rename 1 asset file and replace all references to the asset with the new filename"`
+	RenameAssets *cmdRenameAssets `arg:"subcommand:renames" help:"Read a JSON replacement map from file and rename those assets as well as references to the asset with the new filenames"`
 }
 
 type cmdRenameAsset struct {
@@ -19,17 +23,29 @@ type cmdRenameAsset struct {
 	New   string `arg:"required,positional" help:"New asset filename"`
 }
 
+type cmdRenameAssets struct {
+	Force bool   `arg:"-f,--force" help:"Ignore warning such as mismatch file extensions"`
+	Path  string `arg:"required,positional" help:"Path to replacement map in JSON"`
+}
+
 const basePath = "./assets"
 
 func main() {
 	c := cli{}
 	arg.MustParse(&c)
+
+	var err error
 	switch {
 	case c.RenameAsset != nil:
-		err := c.RenameAsset.run()
-		if err != nil {
-			panic(err)
-		}
+		err = c.RenameAsset.run()
+	case c.RenameAssets != nil:
+		err = c.RenameAssets.run()
+	default:
+		err = errors.New("unmatched subcommand")
+	}
+
+	if err != nil {
+		panic(err)
 	}
 }
 
@@ -38,9 +54,57 @@ func (c *cmdRenameAsset) run() error {
 	if extOld != extNew && !c.Force {
 		return fmt.Errorf("mismatch extension '%s' vs '%s' from src '%s' and dst '%s'", extOld, extNew, c.Old, c.New)
 	}
+	return renameWithReference(c.Old, c.New)
+}
 
-	pathOld := filepath.Join(basePath, c.Old)
-	pathNew := filepath.Join(basePath, c.New)
+func (c *cmdRenameAssets) run() error {
+	j, err := os.ReadFile(c.Path)
+	if err != nil {
+		return fmt.Errorf("failed to read replacement JSON from file '%s': %w", c.Path, err)
+	}
+	var m map[string]string
+	err = json.Unmarshal(j, &m)
+	if err != nil {
+		return fmt.Errorf("failed to marshal replacement JSON from file '%s': %w", c.Path, err)
+	}
+
+	// Deterministic runs, hence slice
+	s := make([][2]string, len(m))
+	i := 0
+	for old, new := range m {
+		if old == "" {
+			return fmt.Errorf("found empty replacement key")
+		}
+		if new == "" {
+			return fmt.Errorf("found empty replacement value")
+		}
+		extK, extV := filepath.Ext(old), filepath.Ext(new)
+		if extK != extV && !c.Force {
+			return fmt.Errorf("mismatch extension '%s' vs '%s' from src '%s' and dst '%s'", extK, extV, old, new)
+		}
+
+		s[i] = [2]string{old, new}
+		i++
+	}
+
+	sort.Slice(s, func(i, j int) bool {
+		return s[0][0] < s[1][0]
+	})
+
+	for i := range s {
+		old, new := s[i][0], s[i][1]
+		err := renameWithReference(old, new)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func renameWithReference(old, new string) error {
+	pathOld := filepath.Join(basePath, old)
+	pathNew := filepath.Join(basePath, new)
 
 	statOld, err := os.Stat(pathOld)
 	if err != nil {
@@ -61,11 +125,11 @@ func (c *cmdRenameAsset) run() error {
 	if err != nil {
 		return fmt.Errorf("failed to copy data to '%s': %w", pathNew, err)
 	}
-	err = far.FindAndReplace("pages", c.Old, c.New)
+	err = far.FindAndReplace("pages", old, new)
 	if err != nil {
 		return fmt.Errorf("failed to find and replace in pages: %w", err)
 	}
-	err = far.FindAndReplace("journals", c.Old, c.New)
+	err = far.FindAndReplace("journals", old, new)
 	if err != nil {
 		return fmt.Errorf("failed to find and replace in journals: %w", err)
 	}
